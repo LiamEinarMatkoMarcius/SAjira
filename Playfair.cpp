926a54d64e8174d8593fb08ca0cde2fee2181dd0
LiamEinarMatkoMarcius <liameinar.matkomarcius@gmail.com>
2025-05-05 10:12:47 +0200

    encryption logic

--- Playfair.cpp
+++ Playfair.cpp
@@ -46,7 +46,89 @@ void convertToAsciiString(string& str) {
 	if (!result.empty()) result.pop_back();
 	str = result;
 }
+void printencryptedMessage(const vector<array<int, 2>>& encryptedMessage, string state) {
+	cout << "" << state << endl;
+	for (const auto& pair : encryptedMessage) {
+		cout << pair[0] << " " << pair[1] << " ";
+	}
+	cout << endl;
+}
+pair<int, int> getCoordinates(int keyMatrix[16][16], int target) {
+	for (int i = 0; i < 16; ++i) {
+		for (int j = 0; j < 16; ++j) {
+			if (keyMatrix[i][j] == target) {
+				return { i, j }; 
+			}
+		}
+	}
+	return { -1, -1 };
+}
+void writeEncryptedToFileAscii(const vector<array<int, 2>>& encryptedMessage, const string& filename) {
+	ofstream outputFile(filename);
 
+	if (!outputFile.is_open()) {
+		cerr << "Error opening file!" << endl;
+		return;
+	}
+
+	for (const auto& pair : encryptedMessage) {
+		outputFile << static_cast<char>(pair[0]) << static_cast<char>(pair[1]);
+	}
+
+	outputFile.close();
+}
+vector<array<int, 2>> encrypt(string message, int keymatrix[16][16]) {
+	vector<array<int, 2>> procMessage; 
+	vector<array<int, 2>> encryptedMessage;
+ 
+	int i = 0;
+	while (i < message.length()) {
+		char first = message[i];
+		char second;
+ 
+		if (i + 1 >= message.length()) {
+			second = 3; 
+			i++;
+		}
+		else if (message[i] == message[i + 1]) {
+			second = 0; 
+			i++;
+		}
+		else {
+			second = message[i + 1];
+			i += 2;
+		}
+		procMessage.push_back({ { static_cast<int>(first), static_cast<int>(second) } });
+ 
+	}
+	printencryptedMessage(procMessage, "po preprocesiranju ");
+	//šifriranje parov
+	for (const array<int, 2>&a : procMessage) {
+		//   i    j
+		pair<int, int> first = getCoordinates(keymatrix, a[0]); // vrstica - stolpec
+		//   k    f
+		pair<int, int> second = getCoordinates(keymatrix, a[1]);
+		if (first.first == second.first) { // enaka vrstica
+			int newChar1 = keymatrix[first.first][(first.second + 1) % 16];
+			int newChar2 = keymatrix[second.first][(second.second + 1) % 16];
+			encryptedMessage.push_back({ newChar1, newChar2 });
+		}
+		else if (first.second == second.second) { // enak stolpec
+			int newChar1 = keymatrix[(first.first + 1) % 16][first.second];
+			int newChar2 = keymatrix[(second.first + 1) % 16][second.second];
+			encryptedMessage.push_back({ newChar1, newChar2 });
+		}
+		else { // razlicna vrstica in stolpec
+ 
+			int newChar1 = keymatrix[first.first][second.second];
+			int newChar2 = keymatrix[second.first][first.second];
+ 
+			encryptedMessage.push_back({ newChar1, newChar2 });
+		}
+	}
+	printencryptedMessage(encryptedMessage, "po šifriranju parov");
+	return encryptedMessage;
+ }
 int main(int argc, const char* argv[]) {
     
 }
